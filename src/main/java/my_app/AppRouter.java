a3212b64143c8bb755e81f215adf51e7d887828d
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2026-03-30 19:15:16 -0300

    implementação corrigida para hotreload em caso de roteamento

---
+++ src/main/java/my_app/AppRouter.java
@@ -0,0 +1,29 @@
+package my_app;
+
+import megalodonte.router.v3.RouteProps;
+import megalodonte.router.v3.Router;
+import my_app.screens.ConfiguracoesScreen;
+import my_app.screens.FornecedoresScreen;
+import my_app.screens.HomeScreen;
+
+import java.util.Set;
+
+public class AppRouter {
+    public static Router build() {
+        var routes = Set.of(
+                new Router.Route("home", ctx -> new HomeScreen(ctx),
+                        new RouteProps(900, 550, null, false)),
+
+                new Router.Route("fornecedores", ctx -> new FornecedoresScreen(ctx),
+                        new RouteProps(900, 550, "Fornecedores", true)),
+
+                new Router.Route("configuracoes", ctx -> new ConfiguracoesScreen(ctx),
+                        new RouteProps(900, 550, "Tela de configurações", true)),
+
+                new Router.Route("produtos", ctx -> new ConfiguracoesScreen(ctx),
+                        new RouteProps(900, 550, "Listagem de produtos", true))
+        );
+
+        return new Router(routes, "fornecedores");
+    }
+}
